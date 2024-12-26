108e7195c5bb7b5e94f764c51124351a93b723e1
JonSelby Co <jons@selbygroupco.com>
2024-12-26 01:43:22 -0800

    foundation for portfolio pieces

--- react/src/components/Portfolio/Portfolio.tsx
+++ react/src/components/Portfolio/Portfolio.tsx
@@ -1,7 +1,47 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from './Portfolio.module.scss'
+
 const Portfolio = () => {
+    const [hoverProject, setHoverProject] = useState<string | undefined>(undefined)
+    const portfolioList = [
+        {
+            id: "p01",
+            name: 'project1',
+            description: 'project description',
+            link: 'www.nike.com'
+        },
+        {
+            id: "p02",
+            name: 'project2',
+            description: 'project description',
+            link: 'www.nike.com'
+        },
+        {
+            id: "p03",
+            name: 'project3',
+            description: 'project description',
+            link: 'www.nike.com'
+        },
+    ]
     return (
-        <div>
-            Portfolio here 2
+        <div className={styles.wrapper}>
+            {portfolioList.map(project => (
+                <motion.div
+                    key={project.id}
+                    whileHover={{ 
+                        scale: 1.2,
+                    }}
+                    transition={{
+                        duration: 1.2
+                    }}
+                    onMouseEnter={() => setHoverProject(project.id)}
+                    onMouseLeave={() => setHoverProject(undefined)}
+                    style={{ backgroundColor: project.id === hoverProject ? 'lightgray' : 'white' }}
+                > 
+                    {project.id === hoverProject ? "show Me" : project.name}
+                </motion.div>
+            ))}
         </div>
     )
 }
